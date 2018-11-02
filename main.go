6b2c6f88e193302204aa1c4abe08a7b599aa377a
bdurb <brian@rpmgreenville.com>
2018-11-01 17:42:56 -0700

    refactored with a customer writer interface

--- main.go
+++ main.go
@@ -7,14 +7,25 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	io.Copy(os.Stdout, resp.Body)
+
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
 	// bs := make([]byte, 99999)
 	// resp.Body.Read(bs)
 	// fmt.Println(string(bs))
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
